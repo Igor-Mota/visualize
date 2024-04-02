1f19385afdea59c314047bc7210bc1efc7b24e14
igor <igorota999@gmail.com>
2024-04-02 16:53:01 -0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,8 +30,8 @@ export default function Home() {
         <Header />
         <Banner />
         <About />
-        <Plans />
         <Community />
+        <Plans />
         <Solutions />
         <Plans />
         <Functionalities />
